53785fe0c8803048b4115ebba93df48c292b7a10
Robert Hardy <rwh29@pitt.edu>
2016-11-19 13:04:49 -0600

    Formatted arduino code better.

--- Thales.ino
+++ Thales.ino
@@ -2,9 +2,9 @@
 //                        https://en.wikipedia.org/wiki/H_bridge
 // Motor Driving Website: https://arduino-info.wikispaces.com/DC-Motors
 
-#include <Adafruit_GPS.h>
-#include <SoftwareSerial.h>
-#include <SharpIR.h>
+#include <Adafruit_GPS.h> // Adafruit GPS library. Thales gave us an Adafruit Ultimate GPS Shield, so their library works perfectly.
+#include <SoftwareSerial.h> // Software serial for GPS module. This is used to give us another serial port so we can communicate with the GPS shield and computer/radio at the same time.
+#include <SharpIR.h> // Infrared distance sensor library. I think it's just a variable resistor, but regardless, the library works like a charm.
 
 // GPS Initialization
 SoftwareSerial mySerial(3, 2);
@@ -25,6 +25,7 @@ int tempPin = 1;
 
 void setup()
 {
+  // Initialize and turn on status LEDs
   pinMode(redLEDPin, OUTPUT);
   pinMode(greenLEDPin, OUTPUT);
   digitalWrite(redLEDPin, HIGH);
@@ -37,29 +38,18 @@ void setup()
   // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
   GPS.begin(9600);
 
-  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  // uncomment this line to turn on only the "minimum recommended" data
-  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
-  // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
-  // the parser doesn't care about other sentences at this time
-
-  // Set the update rate
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
-  // For the parsing code to work nicely and have time to sort thru the data, and
-  // print it out we don't suggest using anything higher than 1 Hz
-
-  // Request updates on antenna status, comment out to keep quiet
-  GPS.sendCommand(PGCMD_ANTENNA);
-
-  // the nice thing about this code is you can have a timer0 interrupt go off
-  // every 1 millisecond, and read data from the GPS for you. that makes the
-  // loop code a heck of a lot easier!
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
+  GPS.sendCommand(PGCMD_ANTENNA); // Request updates on antenna status, comment out to keep quiet
   useInterrupt(true);
 
+  // Give the GPS module time to initialize itself
   delay(1000);
+  
   // Ask for firmware version
   mySerial.println(PMTK_Q_RELEASE);
+
+  // Turn off status LEDs
   digitalWrite(redLEDPin, LOW);
   digitalWrite(greenLEDPin, LOW);
 }
@@ -98,15 +88,9 @@ void loop()                     // run over and over again
   int distance = sensor.getDistance();
   // if a sentence is received, we can check the checksum, parse it...
   if (GPS.newNMEAreceived()) {
-    // a tricky thing here is if we print the NMEA sentence, or data
-    // we end up not listening and catching other sentences!
-    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
-    //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
-
     if (!GPS.parse(GPS.lastNMEA())) {   // this also sets the newNMEAreceived() flag to false
       return;  // we can fail to parse a sentence in which case we should just wait for another
     }
-
     if (printGPSData) {
       Serial.println();
       Serial.println();
@@ -115,12 +99,12 @@ void loop()                     // run over and over again
       Serial.print(GPS.hour, DEC); Serial.print(':');
       Serial.print(GPS.minute, DEC); Serial.print(':');
       Serial.print(GPS.seconds, DEC); Serial.print('.');
-      Serial.println(GPS.milliseconds);
+      Serial.print(GPS.milliseconds);
+      Serial.println("GMT");
       Serial.print("Date: ");
       Serial.print(GPS.day, DEC); Serial.print('/');
       Serial.print(GPS.month, DEC); Serial.print("/20");
       Serial.println(GPS.year, DEC);
-      Serial.println("No Fix");
       if (GPS.fix) {
         Serial.print("Location: ");
         Serial.print(GPS.latitudeDegrees, 4);
@@ -135,6 +119,8 @@ void loop()                     // run over and over again
         } else {
           Serial.println("Poor Reception");
         }
+      } else {
+        Serial.println("No Fix");
       }
       Serial.print("Infrared Distance: ");
       Serial.print(distance);
@@ -148,6 +134,7 @@ void loop()                     // run over and over again
     }
   }
 
+  // LED control basaed on distance sensor
   if (distance <= 5) {
     tooClose = true;
     digitalWrite(redLEDPin, HIGH);
